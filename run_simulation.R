358eabd9b7b1c8f57ec2435d58a5ab554a9fc2f3
colemonnahan <monnahc@uw.edu>
2015-12-10 09:24:31 -0800

    Add new plot to compare timing of JAGS

--- run_simulation.R
+++ run_simulation.R
@@ -29,6 +29,7 @@ growth.df.long <- melt(growth.df, c('model', 'platform', 'seed', 'Npar', 'Nsims'
 growth.df.long <- ddply(growth.df.long, .(platform, Npar, variable, model), mutate,
                        mean.value=mean(value))
 growth.df.wide <- dcast(growth.df, platform+seed+Npar~model, value.var='time')
+growth.df.wide$time.ratio <- growth.df.wide$growth_t/growth.df.wide$growth
 ## Use this to make function to compare between models later
 g <- ggplot(growth.df.long, aes(Npar, log(value), color=platform)) +
     geom_jitter(position=position_jitter(width=.5, height=0), alpha=.5) +
@@ -39,6 +40,11 @@ g <- ggplot(growth.df.wide, aes(log(growth), log(growth_t), group=platform,
                            color=platform, size=platform=='jags')) +
     geom_point() + geom_abline(intercept=0, slope=1) + ggtitle("Time Comparison")
 ggsave('plots/growth_comparison_time.png', g, width=ggwidth, height=ggheight)
+g <- ggplot(growth.df.wide, aes(Npar, time.ratio, group=platform,
+                           color=platform, size=platform=='jags')) +
+    geom_point() + geom_abline(intercept=1, slope=0)+
+        ggtitle("Time Comparison")
+ggsave('plots/growth_comparison_timeratio.png', g, width=ggwidth, height=ggheight)
 
 ## Parameters for the short chains, long ones (to verify models are the
 ## same) are specified in each file and differ.
