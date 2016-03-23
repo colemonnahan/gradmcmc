129c3de0c9e1b34fca7fb6ddb21d053e153f77da
colemonnahan <monnahc@uw.edu>
2016-03-22 19:52:11 -0700

    Explore the rWishart function for generating MVN data

--- models/mvnd/generate_data.R
+++ models/mvnd/generate_data.R
@@ -4,3 +4,14 @@ covar <- rWishart(n=1, df=df, Sigma=diag(df))[1:Npar,1:Npar,1]
 data <- list(covar=covar, Npar=Npar, x=rep(0, len=Npar))
 inits <- list(list(mu=rnorm(n=Npar, mean=0, sd=sqrt(diag(covar)))/2))
 params.jags <- 'mu'
+
+## cors <- ldply(c(2,5), function(Npar){
+##     ldply(c(5,20,200,2000), function(df){
+##         ldply(1:5, function(seed){
+##             covar <- rWishart(n=1, df=df, Sigma=diag(Npar))[,,1]
+##             mycor <- covar/sqrt(diag(covar) %o% diag(covar))
+##             diag(mycor) <- 0
+##             mycor <- max(abs(mycor))
+##             return(cbind(Npar, df, seed, mycor))})})})
+## ggplot(cors, aes(df, mycor, group=Npar))+ geom_point()
+
