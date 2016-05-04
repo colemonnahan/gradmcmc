31f5cee527a39ab62bba58a71e567baf80d18828
colemonnahan <monnahc@uw.edu>
2016-05-04 12:39:49 -0700

    Add some new adaptation plots

--- make_plots.R
+++ make_plots.R
@@ -2,7 +2,7 @@ m <- c('ss_logistic', 'redkite', 'growth_nc', 'swallows', 'mvnc','mvnd')
 ## g <- ggplot(subset(empirical, platform!='jags' & model %in% m)) +
 ##     geom_point(aes(delta.target, log(samples.per.time))) + facet_wrap('model', scales='free_y')
 ## ggsave('plots/optimal_delta.png', g, width=ggwidth, height=ggheight)
-g <- ggplot(empirical, aes(minESS, y=(minESS-minESS.coda)/minESS))  +
+g <- ggplot(empirical, aes(log(minESS), y=(minESS-minESS.coda)/minESS))  +
     geom_point() + geom_abline(intercept=0,slope=0, col='red') +
       facet_grid(platform~model, scales='free')# + ylim(0,1)+ xlim(0,1)
 ggsave('plots/ESS_comparison.png', g, width=ggwidth, height=ggheight)
@@ -21,12 +21,21 @@ g <- ggplot(subset(mvn.means, model=='mvnd'),
   geom_line() + facet_wrap('platform')
 ggsave('plots/perf_mvnd_simulated.png', g, width=ggwidth,
        height=ggheight)
-g <- ggplot(empirical.means.wide, aes(log10(Npar), max.cor, color=log10(stan_re)>0,
-       size=abs(log10(stan_re)))) + geom_point()
+empirical.means.wide$stan_faster <- empirical.means.wide$stan_re>1
+empirical.means.wide$stan_faster <- empirical.means.wide$stan_re>1
+g <- ggplot(empirical.means.wide, aes(log10(Npar), max.cor)) +
+  geom_point(aes(size=abs(log10(stan_re)), color=stan_faster)) +
+    geom_text(aes(label=model), hjust=1, vjust=2)
 ggsave('plots/perf_by_N_cor.png', g, width=ggwidth,
        height=ggheight)
+## Some adaptation plots
 g <- ggplot(adapt_empirical, aes(model, eps.final)) + geom_point(alpha=.5)
+ggsave('plots/adapt_eps.png', g, width=ggwidth, height=ggheight)
 g <- ggplot(adapt_empirical, aes(model, delta.mean)) + geom_point(alpha=.5)
-g <- ggplot(adapt_empirical, aes(model, nsteps.median)) + geom_point(alpha=.5)
+ggsave('plots/adapt_delta.png', g, width=ggwidth, height=ggheight)
+g <- ggplot(adapt_empirical, aes(model, log10(nsteps.median))) + geom_point(alpha=.5)
+ggsave('plots/adapt_nsteps.png', g, width=ggwidth, height=ggheight)
 g <- ggplot(adapt_empirical, aes(model, max_treedepths)) + geom_point(alpha=.5)
-g <- ggplot(adapt_empirical, aes(eps.final, nsteps.median, color=model)) + geom_point()
+ggsave('plots/adapt_max_treedepths.png', g, width=ggwidth, height=ggheight)
+g <- ggplot(adapt_empirical, aes(eps.final, log10(nsteps.mean), color=model)) + geom_point()
+ggsave('plots/adapt_eps_vs_nsteps.png', g, width=ggwidth, height=ggheight)
