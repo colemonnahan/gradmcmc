752f4c7bdb15ca59d16c3b29892b270225f35115
colemonnahan <monnahc@uw.edu>
2016-08-14 09:07:07 -0700

    Add plot of runtime for models

--- make_plots.R
+++ make_plots.R
@@ -10,6 +10,10 @@ g <- ggplot(empirical, aes(model, y=(minESS/Nsims)))  +
   geom_point()+ ylim(0,.5) + facet_wrap('platform') +
     theme(axis.text.x = element_text(angle = 90))
 ggsave('plots/ESS_percentages.png', g, width=ggwidth, height=ggheight)
+g <- ggplot(empirical, aes(model, y=log(time)))  +
+  geom_point()+ facet_wrap('platform') +
+    theme(axis.text.x = element_text(angle = 90))
+ggsave('plots/runtime.png', g, width=ggwidth, height=ggheight)
 g <- ggplot() + geom_line(data=growth.means,
                           aes(log10(Npar), log10(mean.samples.per.time),
                               group=centered, color=centered)) +
